c15687634ad7a67b38ee4ff7a299ea0df75db423
Martin Edgar Atole <107755242+CS-Martin@users.noreply.github.com>
2025-08-09 00:11:57 +0800

    [INI-36] Created professional section

--- apps/client/src/app/(home)/page.tsx
+++ apps/client/src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { AcademicSection } from './_components/academic-section';
 import { FeatureSection } from './_components/feature-section';
 import { HeroSection } from './_components/hero-section';
 import { HowItWorksSection } from './_components/how-it-works-section';
+import { ProfessionalSection } from './_components/professional-section';
 import { SponsorSection } from './_components/sponsor-section';
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <FeatureSection />
       <HowItWorksSection />
       <AcademicSection />
+      <ProfessionalSection />
     </div>
   );
 }
