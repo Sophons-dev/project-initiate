aa40f05c08ae096aab03753b804daafad5f664cd
martin <meatole.cs@gmail.com>
2025-08-18 22:03:43 +0800

    fix: auth0 prod

--- apps/client/src/lib/auth/auth0.ts
+++ apps/client/src/lib/auth/auth0.ts
@@ -4,9 +4,15 @@ import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
 // Initialize the Auth0 client
 export const auth0 = new Auth0Client({
-  domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
+  domain: process.env.AUTH0_DOMAIN,
   secret: process.env.AUTH0_SECRET,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   appBaseUrl: process.env.APP_BASE_URL,
 });
+
+console.log(process.env.AUTH0_DOMAIN);
+console.log(process.env.AUTH0_SECRET);
+console.log(process.env.AUTH0_CLIENT_ID);
+console.log(process.env.AUTH0_CLIENT_SECRET);
+console.log(process.env.APP_BASE_URL);
