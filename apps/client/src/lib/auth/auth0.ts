c341623902034b501fe0fe86b36dd009d3f773fd
martin <meatole.cs@gmail.com>
2025-08-18 23:02:13 +0800

    fix: middleware invocation error

--- apps/client/src/lib/auth/auth0.ts
+++ apps/client/src/lib/auth/auth0.ts
@@ -2,17 +2,15 @@
 
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
+const appBaseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : process.env.APP_BASE_URL || 'http://localhost:3000';
+
 // Initialize the Auth0 client
 export const auth0 = new Auth0Client({
   domain: process.env.AUTH0_DOMAIN,
   secret: process.env.AUTH0_SECRET,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  appBaseUrl: process.env.APP_BASE_URL,
+  appBaseUrl,
 });
-
-console.log(process.env.AUTH0_DOMAIN);
-console.log(process.env.AUTH0_SECRET);
-console.log(process.env.AUTH0_CLIENT_ID);
-console.log(process.env.AUTH0_CLIENT_SECRET);
-console.log(process.env.APP_BASE_URL);
