994d436ea2a2890b37c42c4823a0ada24cebc6b3
Martin Edgar Atole <107755242+CS-Martin@users.noreply.github.com>
2025-08-18 21:49:06 +0800

    [INI-61] Auth0 Integration (#64)
    * feat(client): created onboarding
    
    * feat: created onboarding
    
    * feat: dashboard hero and content
    
    * style: fix navbar behavior in dashboard
    
    * feat: created separate nav for main page
    
    * fix: dropdown bug
    
    * feat: filter and search
    
    * chore: removed avatar in home-nav
    
    * fix: events carousel responsiveness
    
    * fix: events carousel responsiveness
    
    * feat: integrate auth0

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import { Auth0Provider } from '@auth0/nextjs-auth0';
 
 const poppins = Poppins({
   variable: '--font-poppins',
@@ -22,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${poppins.variable} font-sans antialiased`}>
-        <main>{children}</main>
+        <Auth0Provider>
+          <main>{children}</main>
+        </Auth0Provider>
       </body>
     </html>
   );
