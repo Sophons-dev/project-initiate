6e5fac67be7655819ccd6669aab0ff83feba6655
martin <meatole.cs@gmail.com>
2025-08-19 00:08:00 +0800

    fix: attempt 1 callback uri mismatch

--- apps/client/src/lib/auth/auth0.ts
+++ apps/client/src/lib/auth/auth0.ts
@@ -2,15 +2,5 @@
 
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-const appBaseUrl = process.env.VERCEL_URL
-  ? `https://project-initiate.vercel.app`
-  : process.env.APP_BASE_URL || 'http://localhost:3000';
-
 // Initialize the Auth0 client
-export const auth0 = new Auth0Client({
-  domain: process.env.AUTH0_DOMAIN,
-  secret: process.env.AUTH0_SECRET,
-  clientId: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  appBaseUrl,
-});
+export const auth0 = new Auth0Client();
