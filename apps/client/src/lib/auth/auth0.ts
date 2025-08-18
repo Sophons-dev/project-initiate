163da99e836367fff78f575f51f6f3a88db801e3
martin <meatole.cs@gmail.com>
2025-08-18 21:42:45 +0800

    feat: integrate auth0

--- apps/client/src/lib/auth/auth0.ts
+++ apps/client/src/lib/auth/auth0.ts
@@ -3,4 +3,10 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
 // Initialize the Auth0 client
-export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+  domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
+  secret: process.env.AUTH0_SECRET,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  appBaseUrl: process.env.APP_BASE_URL,
+});
