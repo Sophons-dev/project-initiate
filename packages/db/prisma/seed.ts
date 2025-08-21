a5010d1252773ca1b8dc758707cbc97b52f14374
Jiankarlo Belarmino <jianbel.dev@gmail.com>
2025-08-21 23:10:19 +0800

    [INI-64] Update README for Prisma Setup (#85)
    * feat: updated readme with prisma setup
    
    * feat: added ts-node and typescript as dev dependencies
    
    * feat: added seeder file/script, still needs to be implemented

---
+++ packages/db/prisma/seed.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // TODO: Add seed data here
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    if (typeof globalThis.process !== "undefined") {
+      globalThis.process.exit(1);
+    }
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
