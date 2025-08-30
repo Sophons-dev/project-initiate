ebf24fd15420093add28b524165766107e3087a2
NoelEmaas <emaasnoel01@gmail.com>
2025-08-30 23:13:01 +0800

    feat: update useFilter hook to use value for active filter and enhance filter options with new opportunity and organization filters

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -2,16 +2,17 @@ import { useMemo, useState } from 'react';
 
 type FilterOption<T> = {
   label: string;
+  value: string;
   predicate: (item: T) => boolean;
 };
 
 export function useFilter<T>(data: T[], filters: FilterOption<T>[]) {
-  const [activeFilter, setActiveFilter] = useState(filters[0]?.label ?? '');
+  const [activeFilter, setActiveFilter] = useState(filters[0]?.value ?? '');
   const [searchQuery, setSearchQuery] = useState('');
 
   const filteredData = useMemo(() => {
     return data.filter(item => {
-      const filter = filters.find(f => f.label === activeFilter);
+      const filter = filters.find(f => f.value === activeFilter);
       const matchesFilter = filter ? filter.predicate(item) : true;
 
       const matchesSearch =
