f4566adacc5348663ac63c6644533b1ce120eaf8
meatole <meatole@gbox.adnu.edu.ph>
2025-09-03 15:32:55 +0800

    fix: removed inconsisteny for academic section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <FeatureSection />
       <HowItWorksSection />
       <AcademicSection />
-      <ProfessionalSection />
+      {/* <ProfessionalSection /> */}
       <AnnouncementSection />
     </div>
   );
