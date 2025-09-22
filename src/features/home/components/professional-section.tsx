10168858516694d44a35436baa8408194a1a12fe
Marco D. Mosna <122838600+mosnamarco@users.noreply.github.com>
2025-09-22 23:13:24 +0800

    fix: fix improper academic/career growth journey visuals & removed un… (#231)
    * fix: fix improper academic/career growth journey visuals & removed unecessary animation as per requested
    
    * fix: coderabbit issues

--- src/features/home/components/professional-section.tsx
+++ src/features/home/components/professional-section.tsx
@@ -11,18 +11,15 @@ export const ProfessionalSection = () => {
   return (
     <section id='professionals' className='py-16 px-4 lg:px-6'>
       <div className='container max-w-7xl mx-auto'>
-        {/* Reverse grid */}
         <div className='flex flex-col-reverse md:flex-row gap-6 lg:gap-12 items-center'>
           <motion.div className='relative md:w-1/2'>
-            <motion.div whileHover={{ scale: 1.02, rotate: 1 }} transition={{ duration: 0.3 }}>
-              <Image
-                src='/landing-page/for-professionals.png'
-                alt='Students collaborating'
-                width={600}
-                height={600}
-                className='rounded-2xl'
-              />
-            </motion.div>
+            <Image
+              src='/landing-page/for-professionals.png'
+              alt='Professionals collborating'
+              width={600}
+              height={600}
+              className='rounded-2xl'
+            />
             <motion.div className='md:absolute md:left-1/2 md:-translate-x-1/2 md:-bottom-10 w-full mt-5 md:mt-0'>
               <div className='flex flex-col md:flex-row justify-between mx-auto md:w-lg w-full md:gap-4 gap-2 rounded-xl'>
                 <div className='bg-white md:w-1/2 w-full flex flex-row md:items-center space-x-3 p-3 rounded-xl shadow-lg'>
@@ -47,21 +44,15 @@ export const ProfessionalSection = () => {
             </motion.div>
           </motion.div>
 
-          <div className='md:w-1/2 border border-red-500'>
-            <motion.div whileHover={{ scale: 1.05 }} transition={{ duration: 0.3 }}>
-              <TextPill
-                className='mx-0 mb-8 mt-0 !text-xs text-orange-500 b'
-                icon={<GraduationCap className='w-4 h-4' />}
-                text='For Professionals'
-              />
-            </motion.div>
+          <div className='md:w-1/2'>
+            <TextPill
+              className='mx-0 mb-8 mt-0 !text-xs text-orange-500'
+              icon={<GraduationCap className='w-4 h-4' />}
+              text='For Professionals'
+            />
 
             <h2 className='text-3xl lg:text-4xl md:max-w-md text-black mb-6'>
-              Accelerate your{' '}
-              <motion.span className='text-orange-500' whileHover={{ scale: 1.05 }} transition={{ duration: 0.3 }}>
-                Career Growth
-              </motion.span>{' '}
-              with AI
+              Accelerate your <span className='text-orange-500'>Career Growth</span> with AI
             </h2>
 
             <div className='space-y-4 mb-8'>
@@ -70,12 +61,11 @@ export const ProfessionalSection = () => {
                 'Salary insights and other important details',
                 'Executive coaching and leadership development',
               ].map((item, index) => (
-                <div key={index} className='flex items-center space-x-3 '>
+                <div key={index} className='flex items-center space-x-3'>
                   <div className='w-6 h-6 bg-orange-600 rounded-full flex items-center justify-center'>
                     <Check className='w-4 h-4 text-white' />
                   </div>
-
-                  <span className=' text-sm md:text-base'>{item}</span>
+                  <span className='text-sm md:text-base'>{item}</span>
                 </div>
               ))}
             </div>
